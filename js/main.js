f1deda1ba8a23f102f8fae2de97d60ae7d08edb8
rootkitlink <rootkitlink@mail.ru>
2020-06-22 07:56:43 +0300

    v.1

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+  $('.slider').slick({
+    prevArrow: '<button class="slider__arrows prev__arrow"><img src="images/arrow-prev.svg" alt="prev"></button>',
+    nextArrow: '<button class="slider__arrows next__arrow"><img src="images/arrow-next.svg" alt="next"></button>'
+  });
+
+});
+
